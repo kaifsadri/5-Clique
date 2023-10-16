c59d2a11e9ebe7351b2ba4575fce8a9a660a3cf6
Kai F. Sadri <58483176+kaifsadri@users.noreply.github.com>
2023-10-15 22:53:05 -0400

    Much better implementation using sets

--- 5clique.py
+++ 5clique.py
@@ -1,59 +1,44 @@
 from collections import defaultdict, Counter
 from time import perf_counter
 
-with open("words.txt", "r") as words:
-    lex = list(w.upper() for w in words.read().split() if len(w) == len(set(w)) == 5)
-print(f"\nFound {len(lex)} suitable words in the input provided.")
-
+start = perf_counter()
+L = list()
+S = set()
+for w in open("words.txt", "r").read().split():
+    if len(w) == len(set(w)) == 5:
+        L.append(w.upper())
+        S.add(frozenset(w.upper()))
+print(f"{len(L)} words with 5 unique letters.")
+print(f"{len(S)} unique words not including anagrams.")
 # Giving highest value to the least common letters
 cnts = Counter()
-for word in lex:
+for word in S:
     cnts += Counter(word)
-print(f"Order of letters by frequency is {''.join(cnts)}.")
-
-M = dict((letter, 1 << n) for n, letter in enumerate("".join(cnts)))
+C = "".join(c[0] for c in cnts.most_common())
+print(f"Order of letters by decreasing frequency is {C}.")
+M = dict((letter, 1 << n) for n, letter in enumerate(C))
 R = dict((v, k) for (k, v) in M.items())
-
 # Calculating bit masks and filtering anagrams
 BitMasks = defaultdict(set)
-for word in lex:
+for word in L:
     bm = 0
     for letter in word:
         bm |= M[letter]
     BitMasks[bm].add(word)
-# putting words with uncommon letters first. This speeds up the search
-L = sorted(BitMasks.keys(), reverse=True)
-# Now start looking
-s = 0
-N = len(L)
-start = perf_counter()
-stamp = perf_counter()
-for n1 in range(N - 4):
-    for n2 in range(n1 + 1, N - 3):
-        if (L[n1] & L[n2]) == 0:
-            for n3 in range(n2 + 1, N - 2):
-                if (L[n1] & L[n3]) == 0 and (L[n2] & L[n3]) == 0:
-                    for n4 in range(n3 + 1, N - 1):
-                        if (
-                            (L[n1] & L[n4]) == 0
-                            and (L[n2] & L[n4]) == 0
-                            and (L[n3] & L[n4]) == 0
-                        ):
-                            for n5 in range(n4 + 1, N):
-                                if (
-                                    (L[n1] & L[n5]) == 0
-                                    and (L[n2] & L[n5]) == 0
-                                    and (L[n3] & L[n5]) == 0
-                                    and (L[n4] & L[n5]) == 0
-                                ):
-                                    s += 1
-                                    print(
-                                        f"\nSolution {s:3d} after {perf_counter()-stamp:.3f}:\n"
-                                        f"{BitMasks[L[n1]]}\t{BitMasks[L[n2]]}"
-                                        f"\t{BitMasks[L[n3]]}\t{BitMasks[L[n4]]}\t{BitMasks[L[n5]]}"
-                                    )
-                                    print(
-                                        f"Excluded from clique: {R[((1<<26)-1)^(L[n1] | L[n2] | L[n3] | L[n4] | L[n5])]}"
-                                    )
-                                    stamp = perf_counter()
-print(f"\n{s} solutions found in {perf_counter()-start:.3f}.\n")
+B = sorted(BitMasks, reverse=True)  # This sorting speeds up finding the first result.
+# Dict mapping all lesser words that do not overlap with a bitmask.
+D = dict()
+for i, word in enumerate(B):
+    D[word] = set(b for b in B[i + 1 :] if b & word == 0)
+N = 0
+for w1 in B:
+    for w2 in (s1 := D[w1]):
+        for w3 in (s2 := s1 & D[w2]):
+            for w4 in (s3 := s2 & D[w3]):
+                for w5 in s3 & D[w4]:
+                    N += 1
+                    print(
+                        f"Solution {N:3d} at {perf_counter()-start:.2f}s: {' '.join(repr(BitMasks[w]) for w in (w1, w2, w3, w4, w5))} "
+                        f"Excludes : {R[((1<<26)-1)^(w1|w2|w3|w4|w5)]}"
+                    )
+print(f"{N} solutions found in {perf_counter() - start:.2f}.")
